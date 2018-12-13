107791a16d7a8236c0053fdb4583db37a290f9b6
Steven Pequeno <steven@pequeno.in>
2018-12-13 11:42:26 -0600

    day 05 complete

--- 2018/day05/src/main.rs
+++ 2018/day05/src/main.rs
@@ -1,43 +1,75 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
+use std::iter::FromIterator;
 
-fn should_react(a: char, b: char) -> bool {
+fn will_react(a: char, b: char) -> bool {
     let a_lower = a.to_lowercase().to_string();
     let b_lower = b.to_lowercase().to_string();
-    if a_lower != b_lower {
-        false
-    } else {
-        (a.is_uppercase() && b.is_lowercase()) || (a.is_lowercase() && b.is_uppercase())
+    (a_lower == b_lower)
+        && ((a.is_uppercase() && b.is_lowercase()) || (a.is_lowercase() && b.is_uppercase()))
+}
+
+fn clean(mut chars: Vec<char>) -> String {
+    let mut i = 0;
+    let mut j = 1;
+
+    let mut made_change = false;
+    loop {
+        if j >= chars.len() {
+            // we've hit the end of the vec, start again. If we've made it this far and haven't
+            // made a change since the last time we reset the flag to false, then no more changes
+            // are needed.
+            if !made_change {
+                break;
+            } else {
+                made_change = false;
+                i = 0;
+                j = 1;
+            }
+        }
+
+        let a = chars[i];
+        let b = chars[j];
+
+        if will_react(a, b) {
+            made_change = true;
+            chars.remove(i);
+            chars.remove(i);
+        }
+
+        i += 1;
+        j += 1;
     }
+    let ans: String = chars.into_iter().collect();
+    format!("{}", ans)
 }
 
 fn part_one() {
     let mut f = File::open("input.txt").unwrap();
     let mut data = String::new();
     f.read_to_string(&mut data).unwrap();
-    let mut original = data.clone();
-    // let chars: Vec<char> = data.chars().collect();
+    let chars: Vec<char> = data.chars().collect();
+    let ans = clean(chars);
+    println!("{} - {}", ans.len(), ans);
+}
 
-    'outer: loop {
-        let chars: Vec<char> = original.chars().collect();
-        let mut found_match = false;
-        for i in (0..(chars.len() - 1)).step_by(2) {
-            let a = chars[i];
-            let b = chars[i + 1];
-            if should_react(a, b) {
-                found_match = true;
-                let to_remove = format!("{}{}", a, b);
-                original = original.replacen(&to_remove, "", 1);
-                continue 'outer;
-            }
-        }
-        if !found_match {
-            println!("{}", original.len());
-            break;
-        }
+fn part_two() {
+    let mut f = File::open("input2.txt").unwrap();
+    let mut data = String::new();
+    f.read_to_string(&mut data).unwrap();
+    let chars: Vec<char> = data.chars().collect();
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    for letter in alphabet {
+        let upper = letter.to_ascii_uppercase();
+        let test: String = chars.clone().into_iter().collect();
+        let test = test.replace(letter, "").replace(upper, "");
+        let ans = clean(test.chars().collect());
+        println!("{} - {}", letter, ans.len());
     }
 }
 
 fn main() {
-    part_one();
+    // part_one();
+    part_two();
 }
